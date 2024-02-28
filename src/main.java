9d5edb6f4fddf38c39ff115fb756c4b3dade762a
ViniciuMota <vinimota44@gmail.com>
2024-02-28 14:11:20 -0300

    Exercicio coleções parte 1

--- src/main.java
+++ src/main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -11,11 +13,36 @@ public class main {
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Qual a sua idade? ");
-        int idade = scanner.nextInt();
+        List<Pessoa> lista = new ArrayList<>();
+        List<Pessoa> masculino = new ArrayList<>();
+        List<Pessoa> feminino = new ArrayList<>();
+        int res = 1;
 
-        Integer novaIdade = idade;
+        while (res != 0) {
+            System.out.println("Qual o seu nome? ");
+            String nome = scanner.next();
 
-        System.out.println("Sua idade é "+ novaIdade + " anos");
+            System.out.println("Qual o seu gênero? ");
+            String genero = scanner.next();
+
+            Pessoa pessoa = new Pessoa(nome, genero);
+            lista.add(pessoa);
+
+            if (genero.equalsIgnoreCase("masculino")) {
+                masculino.add(pessoa);
+            } else if (genero.equalsIgnoreCase("feminino")) {
+                feminino.add(pessoa);
+            } else {
+                System.out.println("Gênero inválido. Por favor, insira 'masculino' ou 'feminino'.");
+            }
+
+            System.out.println("Adicionar mais uma pessoa?");
+            System.out.println("1 para 'sim' 0 para 'Não'");
+            res = scanner.nextInt();
+        }
+
+        System.out.println("Lista de Pessoas:");
+        System.out.println(masculino);
+        System.out.println(feminino);
     }
 }
